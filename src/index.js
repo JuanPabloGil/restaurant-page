581b2b4369298fb8897adf85fd71197313f929ce
Juan Pablo <jpablomgil@gmail.com>
2020-04-23 17:57:56 -0500

    Adding event listener to hide the content of each section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import _ from 'lodash';
 import {renderHome} from './home.js';
+import {renderNavigation} from './menu.js';
+
+
+
 
 renderHome();
+renderNavigation();
+
+
+const buttonMenu = document.getElementById('buttonMenu');
+buttonMenu.addEventListener('click', (buttonMenu) => {
+  var i, tabcontent;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById("Menu").style.display = "block";
+});
+
+const buttonContact = document.getElementById('buttonContact');
+buttonContact.addEventListener('click', (buttonContact) => {
+  var i, tabcontent;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById("Contact").style.display = "block";
+});
