5c73e9425892e8c920afa91b0082ebbf48723447
Juan Pablo <jpablomgil@gmail.com>
2020-04-24 11:41:56 -0500

    Fix offence eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import _ from 'lodash';
-import {renderHome} from './home.js';
-import {renderNavigation} from './menu.js';
-
-
+import { renderHome } from './home.js';
+import { renderNavigation } from './menu.js';
 
 
 renderHome();
@@ -11,20 +9,22 @@ renderNavigation();
 
 const buttonMenu = document.getElementById('buttonMenu');
 buttonMenu.addEventListener('click', (buttonMenu) => {
-  var i, tabcontent;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  let i; let
+    tabcontent;
+  tabcontent = document.getElementsByClassName('tabcontent');
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = 'none';
   }
-  document.getElementById("Menu").style.display = "block";
+  document.getElementById('Menu').style.display = 'block';
 });
 
 const buttonContact = document.getElementById('buttonContact');
 buttonContact.addEventListener('click', (buttonContact) => {
-  var i, tabcontent;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  let i; let
+    tabcontent;
+  tabcontent = document.getElementsByClassName('tabcontent');
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = 'none';
   }
-  document.getElementById("Contact").style.display = "block";
+  document.getElementById('Contact').style.display = 'block';
 });
--- src/menu.js
+++ src/menu.js
@@ -12,11 +12,80 @@ const renderNavigation = () => {
   //Content
   const menuContent = document.createElement('h1');
   menuContent.className = "content";
-  menuContent.innerHTML = "I'm the menu";
+  menuContent.innerHTML = `
+
+<h1 class="sectionTitle">FOR OUR MENU</h1>
+<div class ="dish">
+  <div class="priceMain">
+    <h6 class="dishTitle">Beef burger meal</h6>
+    <div class="linesPrices"></div>
+    <span class="dishPrice"> $32 </span>
+  </div>
+  <div>
+    <p class= "dishDesc"> Classic greek salad, barrel aged feta cheese, bread</p>
+  </div>
+</div>
+
+<div class ="dish">
+  <div class="priceMain">
+    <h6 class="dishTitle">Roasted lamb rump</h6>
+    <div class="linesPrices"></div>
+    <span class="dishPrice"> $25 </span>
+  </div>
+  <div>
+    <p class= "dishDesc"> Grilled lamb cutlets, pomegranate glaze, butternut squash</p>
+  </div>
+</div>
+
+<div class ="dish">
+  <div class="priceMain">
+    <h6 class="dishTitle">Beef burger meal</h6>
+    <div class="linesPrices"></div>
+    <span class="dishPrice"> $32 </span>
+  </div>
+  <div>
+    <p class= "dishDesc"> Classic greek salad, barrel aged feta cheese, bread</p>
+  </div>
+</div>
+
+<div class ="dish">
+  <div class="priceMain">
+    <h6 class="dishTitle">Beef burger meal</h6>
+    <div class="linesPrices"></div>
+    <span class="dishPrice"> $38 </span>
+  </div>
+  <div>
+    <p class= "dishDesc"> Classic greek salad, barrel aged feta cheese, bread</p>
+  </div>
+</div>
+
+<div class ="dish">
+  <div class="priceMain">
+    <h6 class="dishTitle">Beef burger meal</h6>
+    <div class="linesPrices"></div>
+    <span class="dishPrice"> $41 </span>
+  </div>
+  <div>
+    <p class= "dishDesc"> Classic greek salad, barrel aged feta cheese, bread</p>
+  </div>
+</div>
+
+
+  `;
 
   const contactContent = document.createElement('h1');
   contactContent.className = "content";
-  contactContent.innerHTML = "I'm the contact info";
+  contactContent.innerHTML = `
+
+    <h1 class="sectionTitle"> RESERVE YOUR TABLE </h1>
+    <div class="contactSection">
+      <p> Fray José de Arlegui 705 B, Universitaria, 78290 </p>
+      <p> Reservations@tdr.com </p>
+      <p> Open: 09:00 am – 09:00 pm  </p>
+      <p>  +98 444 817 2568 </p>
+    </div>
+
+  `;
 
 
   //Buttons
@@ -45,4 +114,4 @@ const renderNavigation = () => {
 
 }
 
-export { renderNavigation }
+export  { renderNavigation }
